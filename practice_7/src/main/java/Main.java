00ff8527a393ddf8039c08c1106bacafa08d7a4b
VladMarkov21 <75134893+VladMarkov21@users.noreply.github.com>
2020-12-13 14:18:35 +0300

    Add files via upload

---
+++ practice_7/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayDeque;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game= new Game();
+        ArrayDeque<Integer> first= (ArrayDeque<Integer>) AddDeque.addDeq();
+        ArrayDeque<Integer> second= (ArrayDeque<Integer>) AddDeque.addDeq();
+        Result result=game.game(first,second);
+        System.out.println(result.comand+ " "+ result.kol);
+    }
+}
---
+++ practice_7/src/main/java/Result.java
@@ -0,0 +1,9 @@
+public class Result {
+    public String comand;
+    public int kol;
+    public Result(String comand,int kol){
+        this.comand=comand;
+        this.kol=kol;
+    }
+
+}
